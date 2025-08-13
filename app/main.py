336769610a1df5e79aadf13387f37d2b1fb1cb69
MrYak47 <jacobs.bongo@gmail.com>
2025-08-14 00:31:44 +0200

    Func's updated to used sqlmodel lib/ORM_1408

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 """Module providing a function printing python version."""
 
 from typing import Annotated, Optional
-from datetime import datetime
+from datetime import datetime, timezone
 from random import randrange
 from contextlib import asynccontextmanager
 import psycopg
@@ -82,82 +82,144 @@ async def test_posts(session: SessionDep):
 
 
 @app.get("/posts")
-async def getposts():
-    """Root endpoint that returns a simple greeting message."""
+async def getposts(session: SessionDep):
+    """Root endpoint that returns all posts."""
     # Example of using psycopg3 connection directly
-    with psycopg.connect(dbname="fastapiDb", user="postgres", password="Dyslpostgres") as db_conn:
-        with db_conn.cursor() as cur:
-            cur.execute("""SELECT * FROM public."Posts" """)
-            posts = cur.fetchall()
-            print(posts)
-    return {"data":posts}
+    # with psycopg.connect(dbname="fastapiDb", user="postgres", password="Dyslpostgres") as db_conn:
+    #     with db_conn.cursor() as cur:
+    #         cur.execute("""SELECT * FROM public."Posts" """)
+    #         posts = cur.fetchall()
+    #         print(posts)
+    
+    statement = select(models.Post)
+    results = session.exec(statement).all()
+    
+    return {"data": results}
 
 
 @app.post("/create", status_code=status.HTTP_201_CREATED)
-async def create_post(post: Post):
+async def create_post(post: Post, session: SessionDep):
     """Endpoint to create a post."""
     likes = post.likes if post.likes is not None else randrange(0, 10000)
-    with psycopg.connect(dbname="fastapiDb", user="postgres", password="Dyslpostgres") as db_conn:
-        with db_conn.cursor() as cur:
-            cur.execute("""INSERT INTO public."Posts"("Title", "Content", "Published", "Likes")
-                        VALUES(%s, %s, %s, %s) RETURNING *""",
-                        (post.title, post.content, post.published, likes))
-            postnew = cur.fetchone()
-            db_conn.commit()
-    return {"data": "Post created successfully"}
+    # with psycopg.connect(dbname="fastapiDb", user="postgres", password="Dyslpostgres") as db_conn:
+    #     with db_conn.cursor() as cur:
+    #         cur.execute("""INSERT INTO public."Posts"("Title", "Content", "Published", "Likes")
+    #                     VALUES(%s, %s, %s, %s) RETURNING *""",
+    #                     (post.title, post.content, post.published, likes))
+    #         postnew = cur.fetchone()
+    #         db_conn.commit()
+    
+
+    new_post = models.Post(
+        Title=post.title,
+        Content=post.content,
+        Published=post.published,
+        Likes=likes
+    )
+    
+    session.add(new_post)
+    session.commit()
+    session.refresh(new_post)
+    return {"data": new_post}
 
 
 @app.get("/posts/{post_id}")
-async def get_post(post_id: int):
+async def get_post(post_id: int, session: SessionDep):
     """Endpoint to retrieve a post by its ID."""
-    with psycopg.connect(dbname="fastapiDb", user="postgres", password="Dyslpostgres") as db_conn:
-        with db_conn.cursor() as cur:
-            cur.execute("""SELECT * FROM public."Posts" WHERE "Id" = %s """, (str(post_id),))
-            post = cur.fetchone()
-            if not post :
-                raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                    detail="Post not found"
-                )
-            return {"data": post}
+    # with psycopg.connect(dbname="fastapiDb", user="postgres", password="Dyslpostgres") as db_conn:
+    #     with db_conn.cursor() as cur:
+    #         cur.execute("""SELECT * FROM public."Posts" WHERE "Id" = %s """, (str(post_id),))
+    #         post = cur.fetchone()
+    #         if not post :
+    #             raise HTTPException(
+    #                 status_code=status.HTTP_404_NOT_FOUND,
+    #                 detail="Post not found"
+    #             )
+    #         return {"data": post}
     # response.status_code = status.HTTP_404_NOT_FOUND
-    # return {"error": "Post not found"}, 404
+    
+    statement = select(models.Post).where(models.Post.Id == post_id)
+    post = session.exec(statement).first()
+    
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found"
+        )
+    return {"data": post}
+
 
 
 @app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_post(post_id: int):
+async def delete_post(post_id: int, session: SessionDep):
     """Endpoint to delete a post by its ID."""
-    with psycopg.connect(dbname="fastapiDb", user="postgres", password="Dyslpostgres") as db_conn:
-        with db_conn.cursor() as cur:
-            cur.execute("""DELETE FROM public."Posts" WHERE "Id" = %s RETURNING *""", (str(post_id),))
-            del_post = cur.fetchone()
-            db_conn.commit()
-            if not del_post:
-                raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                    detail="Post not found"
-                )
-            return {"data": del_post}
+    # with psycopg.connect(dbname="fastapiDb", user="postgres", password="Dyslpostgres") as db_conn:
+    #     with db_conn.cursor() as cur:
+    #         cur.execute("""DELETE FROM public."Posts" WHERE "Id" = %s RETURNING *""",
+    #           (str(post_id),))
+    #         del_post = cur.fetchone()
+    #         db_conn.commit()
+    #         if not del_post:
+    #             raise HTTPException(
+    #                 status_code=status.HTTP_404_NOT_FOUND,
+    #                 detail="Post not found"
+    #             )
+    #         return {"data": del_post}
+
+
+    statement = select(models.Post).where(models.Post.Id == post_id)
+    result = session.exec(statement)
+    post = result.first()
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found"
+        )
+    print(post)
+    session.delete(post)
+    session.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 
 @app.put("/posts/{post_id}")
-async def update_post(post_id: int, post: Post):
+async def update_post(post_id: int, post: Post, session: SessionDep):
     """Endpoint to update a post by its ID."""
-    likes = post.likes if post.likes is not None else randrange(0, 10000)
-    with psycopg.connect(dbname="fastapiDb", user="postgres", password="Dyslpostgres") as db_conn:
-        with db_conn.cursor() as cur:
-            cur.execute("""UPDATE public."Posts" 
-                        SET "Title" = %s, "Content" = %s, "Published" = %s, "Likes" = %s
-                        WHERE "Id" = %s
-                        RETURNING *""", (post.title, post.content, post.published, likes, str(post_id))
-                        )
-            updated_post = cur.fetchone()
-            db_conn.commit()
+    # likes = post.likes if post.likes is not None else randrange(0, 10000)
+    # with psycopg.connect(dbname="fastapiDb", user="postgres", password="Dyslpostgres") as db_conn:
+    #     with db_conn.cursor() as cur:
+    #         cur.execute("""UPDATE public."Posts" 
+    #                     SET "Title" = %s, "Content" = %s, "Published" = %s, "Likes" = %s
+    #                     WHERE "Id" = %s
+    #                     RETURNING *""", (post.title, post.content, post.published, likes, str(post_id))
+    #                     )
+    #         updated_post = cur.fetchone()
+    #         db_conn.commit()
 
-            if update_post is None:
-                raise HTTPException(
-                    status_code=status.HTTP_404_NOT_FOUND,
-                    detail="Post not found"
-                )
-            return {"data": updated_post}
+    #         if update_post is None:
+    #             raise HTTPException(
+    #                 status_code=status.HTTP_404_NOT_FOUND,
+    #                 detail="Post not found"
+    #             )
+    #         return {"data": updated_post}
+
+    up_post = session.get(models.Post, post_id)
+   
+    if not up_post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Post not found"
+        )
+    
+    
+    up_post.Title = post.title
+    up_post.Content = post.content
+    up_post.Published = post.published
+    up_post.Likes = post.likes if post.likes is not None else randrange(0, 10000)
+    
+    print(up_post)
+    session.add(up_post)
+    session.commit()
+    session.refresh(up_post)
+    return {"data": up_post}
 
--- app/models.py
+++ app/models.py
@@ -13,11 +13,11 @@ from sqlmodel import Field, Session, SQLModel, create_engine, select, Column
 
 class Post(SQLModel, table=True):
     """Model representing a post."""
-    __tablename__ = "posts"
+    __tablename__ = "Posts"
     
-    id: int = Field(default=None, primary_key=True, nullable=False)
-    title: str = Field(nullable=False, max_length=100)
-    content: str = Field(nullable=False, max_length=500)
-    published: bool = Field(sa_column_kwargs={"server_default": str("False")})
-    created_by: datetime =  Field(sa_column=Column(DateTime, nullable=True, server_default=text("(now() AT TIME ZONE 'UTC')")))
-    likes: int = Field(default=randrange(0, 10000), nullable=True)
+    Id: int = Field(default=None, primary_key=True, nullable=False)
+    Title: str = Field(nullable=False, max_length=100)
+    Content: str = Field(nullable=False, max_length=500)
+    Published: bool = Field(sa_column_kwargs={"server_default": str("False")})
+    Created_by: datetime =  Field(sa_column=Column(DateTime, nullable=True, server_default=text("(now() AT TIME ZONE 'UTC')")))
+    Likes: int = Field(default=randrange(0, 10000), nullable=True)
